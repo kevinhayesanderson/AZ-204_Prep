fda4cad36f2278213b141348c31436604dbb3f0b
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-07-14 19:31:58 +0530

    update acr management code

--- Develop_Azure_compute_solutions/Implement_containerized_solutions/containerManagement/Program.cs
+++ Develop_Azure_compute_solutions/Implement_containerized_solutions/containerManagement/Program.cs
@@ -4,18 +4,44 @@ using Azure.Identity;
 
 
 
-// Get the service endpoint from the environment
-Uri endpoint = new Uri(Environment.GetEnvironmentVariable("REGISTRY_ENDPOINT"));
 
-// Create a new ContainerRegistryClient
-ContainerRegistryClient client = new ContainerRegistryClient(endpoint, new DefaultAzureCredential());
+// Get the service endpoint from the environment
+// Create a ContainerRegistryClient that will authenticate to your registry through Azure Active Directory
+Uri endpoint = new Uri("https://myregistry.azurecr.io");
+ContainerRegistryClient client = new ContainerRegistryClient(endpoint, new DefaultAzureCredential(),
+    new ContainerRegistryClientOptions()
+    {
+        Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud
+    });
 
 // Get the collection of repository names from the registry
-AsyncPageable<string> repositories = client.GetRepositoryNamesAsync();
-await foreach (string repository in repositories)
+//Set artifact properties asynchronously
+AsyncPageable<string> repositoryNames = client.GetRepositoryNamesAsync();
+await foreach (string repositoryName in repositoryNames)
 {
-    Console.WriteLine(repository);
+    ContainerRepository repository = client.GetRepository(repositoryName);
+
+    // Obtain the images ordered from newest to oldest
+    AsyncPageable<ArtifactManifestProperties> imageManifests =
+        repository.GetAllManifestPropertiesAsync(manifestOrder: ArtifactManifestOrder.LastUpdatedOnDescending);
+
+    // Delete images older than the first three.
+    foreach (ArtifactManifestProperties imageManifest in imageManifests.ToBlockingEnumerable().Skip(3))
+    {
+        RegistryArtifact image = repository.GetArtifact(imageManifest.Digest);
+        Console.WriteLine($"Deleting image with digest {imageManifest.Digest}.");
+        Console.WriteLine($"Deleting the following tags from the image:");
+        foreach (var tagName in imageManifest.Tags)
+        {
+            Console.WriteLine($"{imageManifest.RepositoryName}:{tagName}");
+            await image.DeleteTagAsync(tagName);
+        }
+        await image.DeleteAsync();
+    }
 }
+
+
+
 
 //Handle Errors
 string fakeRepositoryName = "doesnotexist";
